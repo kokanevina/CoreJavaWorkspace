72cfb83c5e8934a83d146a86a9dfca64cce1df7d
kokanevina <kokane.vina@gmail.com>
2022-08-12 17:53:19 +0530

    Finally

--- Exception Handling/src/maths/MathOperation.java
+++ Exception Handling/src/maths/MathOperation.java
@@ -20,17 +20,24 @@ public class MathOperation {
 		div= a/b;     // ArithmeticException object
 		double result=div+7;
 		System.out.println("this stmt gets skipped if exception occurs above");
+		
 		}
-		catch(ArithmeticException ae) {
-				System.out.println("Exception occured");
-				System.out.println(ae);  // toString
-		}
-		catch(Exception e) {
+		catch(ArrayIndexOutOfBoundsException ae) {
 			System.out.println("Exception occured");
+			System.out.println(ae);  // toString
+		 }
+		catch(Exception e) {
+			System.out.println("Exception handled in Last catch");
 			System.out.println(e); // toString of subclass
+		} /*super class catch should be below subclass catch*/
+		finally {
+			/* release the resources in finally block*/
+			/*file closing, scanner closing, db connection closing*/
+			System.out.println("this is stmt is very much important");
+			System.out.println("finally always gets excuted thought excpetion is there or not");
 		}
 		System.out.println("normal execution contiues from here");
 		return div;
 	}
-	
+	/*try with resources : try(open the resources here)*/
 }
