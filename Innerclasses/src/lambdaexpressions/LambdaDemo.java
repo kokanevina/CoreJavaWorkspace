b6d27137120c96c608cdb33259511ceb2751e01d
kokanevina <kokane.vina@gmail.com>
2022-08-25 15:44:25 +0530

    Lambda Expressions

--- Innerclasses/src/lambdaexpressions/LambdaDemo.java
+++ Innerclasses/src/lambdaexpressions/LambdaDemo.java
@@ -1,12 +1,7 @@
 package lambdaexpressions;
 // interface having only one method declaration: Functional Interface
-interface MathIntf{
-	 double square(double num);
-}
-
 public class LambdaDemo {
 public static void main(String[] args) {
-	
 	MathIntf math=new MathIntf() {
 		@Override
 		public double square(double num) {
@@ -16,21 +11,17 @@ public static void main(String[] args) {
 	};
 	double res=math.square(3);
 	System.out.println("Square is :"+res);
-	
 	// ()->{}  : thin arrow 
-	
 	MathIntf math2= (double num)->{return num*num;};
 	double res1=math2.square(4);
 	System.out.println("Square is :"+res1);
 	// lets remove optional things]
-	
 	/*1. no need to write datatype of parameter
 	 * 2. if single parameter only : then () are optional
 	 * 3. if single stmt : {} optional
 	 * 4. if single return sttmt then : {} optional, return keyword not allowed, function automatically
 	 * returns a result*/
-	
-	
+
 	MathIntf math3= num->num*num;
 	double res2=math3.square(4);
 	System.out.println("Square is :"+res2);
