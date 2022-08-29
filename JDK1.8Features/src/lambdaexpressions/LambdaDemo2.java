a35b508a69e8c8395a2af12edbdd32f86823a3e3
kokanevina <kokane.vina@gmail.com>
2022-08-29 11:37:22 +0530

    Method Reference

--- JDK1.8Features/src/lambdaexpressions/LambdaDemo2.java
+++ JDK1.8Features/src/lambdaexpressions/LambdaDemo2.java
@@ -1,11 +1,31 @@
 package lambdaexpressions;
 
+class ChildWork{
+	
+	public static void loop(){
+		for(int k=10;k<=20;k++) {
+			System.out.println(k);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+
+
+
+
 public class LambdaDemo2 {
 public static void main(String[] args) {
 	
 	//1. Runnable obj
 	//2. Thread obj
 	//3. start a thread
+	/*
 	Runnable r=()-> {
 		System.out.println("Child 1 started executing");
 		for(int i=1;i<=5;i++) {
@@ -37,5 +57,15 @@ public static void main(String[] args) {
 	Runnable r2=()-> System.out.println("Child started executing");
 	Thread thread2=new Thread(r2);
 	thread2.start();
+	
+*/
+	Runnable r3=ChildWork::loop;
+	Thread t3=new Thread(r3);
+	t3.start();
+	
+	
+	
+	
+	
 }
 }
---
+++ JDK1.8Features/src/methodreference/Voter.java
@@ -0,0 +1,10 @@
+package methodreference;
+
+public class Voter {
+	public boolean checkAge(int age) {
+		if(age>=18)
+			return true;
+		else 
+			return false;
+	}
+}
